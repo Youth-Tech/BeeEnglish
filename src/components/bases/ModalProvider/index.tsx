6d1034d0ce363a7861f2ec098c912b82f629861f
raikojima <duyvo761@gmail.com>
2023-08-27 22:47:34 +0700

    feat: modal component

---
+++ src/components/bases/ModalProvider/index.tsx
@@ -0,0 +1,182 @@
+import { Pressable, StyleSheet } from 'react-native'
+import React from 'react'
+import { ModalProviderProps } from './type'
+import { Block } from '@components'
+import { heightScreen, widthScreen } from '@utils/helpers'
+import { useTheme } from '@themes'
+import Animated, {
+  runOnJS,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+  withTiming,
+} from 'react-native-reanimated'
+import { SpringConfig } from 'react-native-reanimated/lib/typescript/reanimated2/animation/springUtils'
+
+export const ModalProvider = React.forwardRef<any, ModalProviderProps>(
+  (props, ref) => {
+    const {
+      children,
+      position = 'bottom',
+      modalHeight = 279,
+      animationType,
+      modalComponent,
+      backDropComponent,
+      onShow,
+      onDismiss,
+    } = props
+    const { colors } = useTheme()
+    const [_visible, set_visible] = React.useState<boolean>(false)
+    const AnimatedBlock = Animated.createAnimatedComponent(Block)
+    const AnimatedPressable = Animated.createAnimatedComponent(Pressable)
+    const ModalSpringConfig: SpringConfig = {
+      mass: 1,
+      damping: 15,
+      stiffness: 100,
+      overshootClamping: false,
+      restDisplacementThreshold: 0.01,
+      restSpeedThreshold: 3,
+    }
+    const BOTTOM_AND_TOP_POSITION = 30
+    const CENTER_POSITION = heightScreen / 2 - modalHeight / 2
+    const opacity = useSharedValue(animationType !== 'fade' ? 1 : 0)
+    const slideTo = useSharedValue(-modalHeight)
+
+    const rOpacityStyle = useAnimatedStyle(() => {
+      return {
+        opacity: opacity.value,
+      }
+    })
+    const rSlideBottomStyle = useAnimatedStyle(() => {
+      return {
+        bottom: slideTo.value,
+      }
+    })
+    const rSlideTopnCenterStyle = useAnimatedStyle(() => {
+      return {
+        top: slideTo.value,
+      }
+    })
+    const handleDismiss = () => {
+      if (animationType === 'fade') {
+        opacity.value = withTiming(0, undefined, (isFinished) => {
+          if (isFinished) {
+            runOnJS(handleSetVisible)(false)
+          }
+        })
+      } else {
+        slideTo.value = withTiming(
+          -modalHeight,
+          { duration: 200 },
+          (isFinished) => {
+            if (isFinished) {
+              runOnJS(handleSetVisible)(false)
+            }
+          },
+        )
+      }
+      onDismiss?.()
+    }
+    const handleOpen = () => {
+      handleSetVisible(true)
+      if (animationType === 'fade') {
+        opacity.value = withTiming(1)
+      } else if (animationType === 'slide') {
+        slideTo.value = withSpring(
+          position === 'center' ? CENTER_POSITION : BOTTOM_AND_TOP_POSITION,
+          ModalSpringConfig,
+        )
+      }
+      onShow?.()
+    }
+    React.useImperativeHandle(
+      ref,
+      () => {
+        return {
+          openModal() {
+            handleOpen()
+          },
+          dismissModal() {
+            handleDismiss()
+          },
+        }
+      },
+      [],
+    )
+    const handleSetVisible = (visible: boolean) => {
+      set_visible(visible)
+    }
+
+    return (
+      <Block flex>
+        {children}
+        {_visible && (
+          <AnimatedPressable
+            backgroundColor="rgba(0,0,0,0.5)"
+            style={[StyleSheet.absoluteFill, rOpacityStyle]}
+            onPress={handleDismiss}
+          >
+            <Block flex>{backDropComponent}</Block>
+          </AnimatedPressable>
+        )}
+        {position === 'bottom' && _visible && (
+          <AnimatedBlock
+            absolute
+            width={widthScreen - 40}
+            height={modalHeight}
+            backgroundColor={colors.white}
+            radius={15}
+            alignSelf="center"
+            style={[
+              animationType === 'fade' ? rOpacityStyle : rSlideBottomStyle,
+              animationType === 'fade'
+                ? { bottom: BOTTOM_AND_TOP_POSITION }
+                : { bottom: -modalHeight },
+            ]}
+          >
+            {modalComponent}
+          </AnimatedBlock>
+        )}
+        {position === 'top' && _visible && (
+          <AnimatedBlock
+            absolute
+            width={widthScreen - 40}
+            height={modalHeight}
+            backgroundColor={colors.white}
+            radius={15}
+            alignSelf="center"
+            style={[
+              animationType === 'fade' ? rOpacityStyle : rSlideTopnCenterStyle,
+              animationType === 'fade'
+                ? { top: BOTTOM_AND_TOP_POSITION }
+                : { top: -modalHeight },
+            ]}
+          >
+            {modalComponent}
+          </AnimatedBlock>
+        )}
+
+        {position === 'center' && _visible && (
+          <AnimatedBlock
+            absolute
+            width={widthScreen - 40}
+            height={modalHeight}
+            backgroundColor={colors.white}
+            radius={15}
+            alignSelf="center"
+            style={[
+              animationType === 'fade' ? rOpacityStyle : rSlideTopnCenterStyle,
+              animationType === 'fade'
+                ? { top: CENTER_POSITION }
+                : { top: -modalHeight },
+            ]}
+          >
+            {modalComponent}
+          </AnimatedBlock>
+        )}
+      </Block>
+    )
+  },
+)
+
+const styles = StyleSheet.create({})
--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback, useRef } from 'react'
 import { Pressable, StyleSheet, ScrollView } from 'react-native'
 import { navigate } from '@navigation'
 import {
@@ -11,149 +11,186 @@ import {
   ShadowButton,
   LinearGradient,
   ShadowBlock,
+  ModalProvider,
 } from '@components'
 
 export const HomeScreen: React.FC = () => {
   const [value, setValue] = React.useState('')
-
+  const modalRef = useRef(null)
+  const ref = useRef(null)
+  const handleOpen = useCallback(() => {
+    modalRef.current?.openModal()
+  }, [])
+  const handleDismiss = useCallback(() => {
+    modalRef.current?.dismissModal()
+  }, [])
+  const onShow = useCallback(() => {
+    console.log('Modal opened')
+  }, [])
+  const onDissmiss = useCallback(() => {
+    console.log('Modal closed')
+  }, [])
   return (
-    <Container backgroundColor="white">
-      <ScrollView contentContainerStyle={{ paddingBottom: 20 }}>
-        <Block flex>
-          <Text size={20} lineHeight={25} color={'primary'} fontFamily="bold">
-            This's a custom Text component
-          </Text>
-          <TextInput label="Xin Chào" value={value} onChangeText={setValue} />
+    <ModalProvider
+      ref={modalRef}
+      position="bottom"
+      animationType="fade"
+      onShow={onShow}
+      onDismiss={onDissmiss}
+      modalComponent={
+        <Block flex alignCenter justifyCenter radius={15}>
           <Pressable
-            onPress={() => {
-              navigate('HOME_SCREEN_1')
-            }}
+            style={{ backgroundColor: 'red', padding: 50 }}
+            onPress={handleDismiss}
           >
-            <Text>Go To Home</Text>
+            <Text>Press me</Text>
           </Pressable>
-          <Progress
-            step={80}
-            strokeHeight={10}
-            totalSteps={100}
-            progressContainerStyles={{
-              paddingHorizontal: 10,
-            }}
-          />
-          <CircleProgress
-            size={150}
-            step={50}
-            totalSteps={100}
-            strokeWidth={10}
-          />
-          <Block width={'50%'}>
-            <ShadowButton
-              labelSize={'h2'}
-              buttonRadius={8}
-              fontFamily="bold"
-              buttonHeight={45}
-              shadowHeight={10}
-              buttonBorderSize={2}
-              buttonColor="#FFEFAD"
-              labelColor="primaryText"
-              shadowButtonColor="#FFC107"
+        </Block>
+      }
+    >
+      <Container backgroundColor="white">
+        <ScrollView contentContainerStyle={{ paddingBottom: 20 }}>
+          <Block flex>
+            <Text size={20} lineHeight={25} color={'primary'} fontFamily="bold">
+              This's a custom Text component
+            </Text>
+            <TextInput
+              ref={ref}
+              label="Xin Chào"
+              value={value}
+              onChangeText={setValue}
+            />
+            <Pressable
               onPress={() => {
-                console.log('press')
+                navigate('HOME_SCREEN_1')
               }}
-              buttonBorderColor={
-                <Block style={StyleSheet.absoluteFill}>
-                  <LinearGradient
-                    colors={['#FFEFAD', '#FFC107']}
-                    containerStyle={{ width: '100%', height: '100%' }}
-                  />
-                </Block>
-              }
-            />
-          </Block>
-          <ShadowBlock
-            row
-            marginTop={20}
-            space="between"
-            paddingHorizontal={20}
-            containerPaddingHorizontal={20}
-          >
-            <Block width={10} height={10} backgroundColor="red" />
+            >
+              <Text>Go To Home</Text>
+            </Pressable>
             <Progress
-              step={10}
-              totalSteps={100}
+              step={80}
               strokeHeight={10}
+              totalSteps={100}
               progressContainerStyles={{
-                flex: 1,
-                marginStart: 10,
+                paddingHorizontal: 10,
               }}
             />
-          </ShadowBlock>
-          <Block row>
+            <CircleProgress
+              size={150}
+              step={50}
+              totalSteps={100}
+              strokeWidth={10}
+            />
+            <Block width={'50%'}>
+              <ShadowButton
+                labelSize={'h2'}
+                buttonRadius={8}
+                fontFamily="bold"
+                buttonHeight={45}
+                shadowHeight={10}
+                buttonBorderSize={2}
+                buttonColor="#FFEFAD"
+                labelColor="primaryText"
+                shadowButtonColor="#FFC107"
+                onPress={() => {
+                  handleOpen()
+                }}
+                buttonBorderColor={
+                  <Block style={StyleSheet.absoluteFill}>
+                    <LinearGradient
+                      colors={['#FFEFAD', '#FFC107']}
+                      containerStyle={{ width: '100%', height: '100%' }}
+                    />
+                  </Block>
+                }
+              />
+            </Block>
             <ShadowBlock
               row
-              flex
-              radius={20}
-              height={120}
-              justifyCenter
               marginTop={20}
-              marginLeft={10}
-              marginRight={5}
-              borderWidth={10}
-              shadowPosition="top"
-              shadowLabel="Total XP"
+              space="between"
               paddingHorizontal={20}
-              shadowBackgroundColor="#C584F8"
-              shadowLabelContainerStyle={{
-                justifyEnd: true,
-                alignCenter: true,
-              }}
-              shadowLabelTextStyle={{
-                color: 'white',
-                fontFamily: 'bold',
-                paddingVertical: 10,
-                size: 'h1',
-              }}
+              containerPaddingHorizontal={20}
             >
-              <Text fontFamily="bold" size="h2" color="#C584F8">
-                200
-              </Text>
+              <Block width={10} height={10} backgroundColor="red" />
+              <Progress
+                step={10}
+                totalSteps={100}
+                strokeHeight={10}
+                progressContainerStyles={{
+                  flex: 1,
+                  marginStart: 10,
+                }}
+              />
             </ShadowBlock>
-            <ShadowBlock
-              row
-              flex
-              radius={20}
-              height={120}
-              justifyCenter
-              marginTop={20}
-              marginLeft={5}
-              marginRight={10}
-              borderWidth={10}
-              shadowPosition="top"
-              shadowLabel="Total XP"
-              paddingHorizontal={20}
-              shadowBackgroundColor="#C584F8"
-              shadowLabelContainerStyle={{
-                justifyEnd: true,
-                alignCenter: true,
-              }}
-              shadowLabelTextStyle={{
-                color: 'white',
-                fontFamily: 'bold',
-                paddingVertical: 10,
-                size: 'h1',
-              }}
-            >
-              <Text fontFamily="bold" size="h2" color="#C584F8">
-                200
-              </Text>
-            </ShadowBlock>
-          </Block>
-          {/* <LinearGradient
+            <Block row>
+              <ShadowBlock
+                row
+                flex
+                radius={20}
+                height={120}
+                justifyCenter
+                marginTop={20}
+                marginLeft={10}
+                marginRight={5}
+                borderWidth={10}
+                shadowPosition="top"
+                shadowLabel="Total XP"
+                paddingHorizontal={20}
+                shadowBackgroundColor="#C584F8"
+                shadowLabelContainerStyle={{
+                  justifyEnd: true,
+                  alignCenter: true,
+                }}
+                shadowLabelTextStyle={{
+                  color: 'white',
+                  fontFamily: 'bold',
+                  paddingVertical: 10,
+                  size: 'h1',
+                }}
+              >
+                <Text fontFamily="bold" size="h2" color="#C584F8">
+                  200
+                </Text>
+              </ShadowBlock>
+              <ShadowBlock
+                row
+                flex
+                radius={20}
+                height={120}
+                justifyCenter
+                marginTop={20}
+                marginLeft={5}
+                marginRight={10}
+                borderWidth={10}
+                shadowPosition="top"
+                shadowLabel="Total XP"
+                paddingHorizontal={20}
+                shadowBackgroundColor="#C584F8"
+                shadowLabelContainerStyle={{
+                  justifyEnd: true,
+                  alignCenter: true,
+                }}
+                shadowLabelTextStyle={{
+                  color: 'white',
+                  fontFamily: 'bold',
+                  paddingVertical: 10,
+                  size: 'h1',
+                }}
+              >
+                <Text fontFamily="bold" size="h2" color="#C584F8">
+                  200
+                </Text>
+              </ShadowBlock>
+            </Block>
+            {/* <LinearGradient
             colors={['#FFEFAD', '#FFC107']}
             containerStyle={{ flex: 1 }}
             transform={{ rotation: 20 }}
           /> */}
-        </Block>
-      </ScrollView>
-    </Container>
+          </Block>
+        </ScrollView>
+      </Container>
+    </ModalProvider>
   )
 }
