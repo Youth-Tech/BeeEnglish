cf91203b29651978be23fb55d11436c8c65883ca
Joseph Nguyễn <129431919+josephanh@users.noreply.github.com>
2023-10-26 20:32:28 +0700

    fix data word

--- src/screens/LearnedWordScreen/const.ts
+++ src/screens/LearnedWordScreen/const.ts
@@ -5,3 +5,47 @@ export interface dataProps {
   translation: string
   difficulty: 'easy' | 'normal' | 'hard'
 }
+export const data: dataProps[] = [
+  {
+    id: 1,
+    word: 'Chicken',
+    wordType: 'noun',
+    translation: 'Con gà',
+    difficulty: 'easy',
+  },
+  {
+    id: 2,
+    word: 'Dog',
+    wordType: 'noun',
+    translation: 'Con chó',
+    difficulty: 'normal',
+  },
+  {
+    id: 3,
+    word: 'Difference',
+    wordType: 'noun',
+    translation: 'Sự khác biệt',
+    difficulty: 'hard',
+  },
+  {
+    id: 4,
+    word: 'Hello',
+    wordType: 'noun',
+    translation: 'Sự khác biệt',
+    difficulty: 'hard',
+  },
+  {
+    id: 5,
+    word: 'Hello',
+    wordType: 'noun',
+    translation: 'Sự khác biệt',
+    difficulty: 'hard',
+  },
+  {
+    id: 6,
+    word: 'Hello',
+    wordType: 'noun',
+    translation: 'Sự khác biệt',
+    difficulty: 'hard',
+  },
+]
--- src/screens/LearnedWordScreen/index.tsx
+++ src/screens/LearnedWordScreen/index.tsx
@@ -14,55 +14,12 @@ import { LearnedWordItem } from './components'
 import { goBack } from '@navigation'
 import { FlatList, ListRenderItemInfo, View } from 'react-native'
 import { widthScreen } from '@utils/helpers'
-import { dataProps } from './const'
+import { data, dataProps } from './const'
 
 export const LearnedWordScreen = () => {
   const { colors, normalize } = useTheme()
   const { t } = useTranslation()
-  const data: dataProps[] = [
-    {
-      id: 1,
-      word: 'Chicken',
-      wordType: 'noun',
-      translation: 'Con gà',
-      difficulty: 'easy',
-    },
-    {
-      id: 2,
-      word: 'Dog',
-      wordType: 'noun',
-      translation: 'Con chó',
-      difficulty: 'normal',
-    },
-    {
-      id: 3,
-      word: 'Difference',
-      wordType: 'noun',
-      translation: 'Sự khác biệt',
-      difficulty: 'hard',
-    },
-    {
-      id: 4,
-      word: 'Hello',
-      wordType: 'noun',
-      translation: 'Sự khác biệt',
-      difficulty: 'hard',
-    },
-    {
-      id: 5,
-      word: 'Hello',
-      wordType: 'noun',
-      translation: 'Sự khác biệt',
-      difficulty: 'hard',
-    },
-    {
-      id: 6,
-      word: 'Hello',
-      wordType: 'noun',
-      translation: 'Sự khác biệt',
-      difficulty: 'hard',
-    },
-  ]
+
   const renderLearnedWordItem = ({
     index,
     item,
@@ -81,7 +38,7 @@ export const LearnedWordScreen = () => {
         <Block flex>
           <Block
             width={widthScreen}
-            height={228}
+            height={208}
             backgroundColor={colors.orangePrimary}
             borderBottomLeftRadius={50}
             borderBottomRightRadius={50}
