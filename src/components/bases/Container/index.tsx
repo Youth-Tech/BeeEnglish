8e723e8aefe2c0b5247a683f0072f2faeef63756
duy vo <duyvo761@gmail.com>
2023-10-26 17:00:57 +0700

    fix status bar height

--- src/components/bases/Container/index.tsx
+++ src/components/bases/Container/index.tsx
@@ -1,12 +1,12 @@
 import React, { FC } from 'react'
 
-import { ViewProps, ScrollView } from 'react-native'
+import { ScrollView, ViewProps } from 'react-native'
 import { Edge, SafeAreaView } from 'react-native-safe-area-context'
 import { makeStyles, useTheme } from '@themes'
 
 import { StatusBar } from '../StatusBar'
 import { Block } from '../Block'
-import { getStatusBarHeight } from '@components/bases/StatusBar/status_bar_height'
+
 export type ContainerProps = {
   statusColor?: string
   edges?: Edge[]
@@ -41,6 +41,5 @@ const useStyles = makeStyles<ContainerProps>()(({}) => ({
   root: ({ backgroundColor }) => ({
     flex: 1,
     backgroundColor: backgroundColor,
-    marginTop: getStatusBarHeight(),
   }),
 }))
