440052214f78a16c193b2af56e519c7d9f74f9f6
TranVux <vutran789jjjj@gmail.com>
2023-12-20 08:27:12 +0700

    change base url ✨

--- src/configs/constants.ts
+++ src/configs/constants.ts
@@ -1,4 +1,4 @@
 // export const BASE_URL = 'https://pokeapi.co/api/v2/'
 // export const BASE_URL = 'http://10.0.2.2:3000/api/v1'
-// export const BASE_URL = 'https://tamng.id.vn/api'
-export const BASE_URL = 'https://0035-113-161-50-174.ngrok-free.app/api/v1'
+export const BASE_URL = 'https://tamng.id.vn/api'
+// export const BASE_URL = 'https://0035-113-161-50-174.ngrok-free.app/api/v1'
